f8f4dbffcd8eddf0d1fc5fdde12beaf395f0dc1d
Khushi thakur <tkhushi837@gmail.com>
2025-07-23 23:11:49 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -1,3 +1,28 @@
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+def create_webdriver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-software-rasterizer")
+    chrome_options.add_argument("--window-size=1920x1080")
+
+    chrome_path = "/usr/bin/google-chrome"
+    driver_path = "/usr/bin/chromedriver"
+
+    chrome_options.binary_location = chrome_path
+
+    service = Service(executable_path=driver_path)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    return driver
+
+
 import os
 import time
 import re
@@ -97,3 +122,4 @@ if st.button("Get Highlights"):
             st.warning(" No relevant highlights found in the 'Key Highlights' section.")
     except Exception as e:
         st.error(f"⚠️ Error: {str(e)}")
+
