5a1ac902ffc4bddd9c18a547a6e07b06a6edd32f
Khushi thakur <tkhushi837@gmail.com>
2025-07-23 22:36:45 +0530

    Update install_chrome.sh

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# install_chrome.sh
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -y ./google-chrome-stable_current_amd64.deb
+# Install Google Chrome
+apt-get update
+apt-get install -y wget curl unzip gnupg
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+apt-get update
+apt-get install -y google-chrome-stable
